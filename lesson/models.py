d0754c3e66d001759f6f61ceaff81489bafd0718
TatSmat <smatova.tatiana@gmail.com>
2021-09-27 23:01:46 +0300

    Iteration 1. Add Material model
    Add Material model
    Add Material to admin pannel

---
+++ lesson/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Material(models.Model):
+
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique_for_date='publish')
+# уникальный двух полей: пара слаг и дата д.б. уникальны
+
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_created=True)
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='user_materials')
+
+    MATERIAL_TYPE = [
+        ('theory', 'Theoretical Materials'),
+        ('practice', 'Practical Materials'),
+    ]
+
+    material_tape = models.CharField(max_length=255,
+                                     choices=MATERIAL_TYPE,
+                                     default='theory')
+
+    def __str__(self):
+        return self.title  # для отображения названий материалов в админке
