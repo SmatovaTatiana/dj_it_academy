9b094a6f9cd4f0644990ac687e41356ff55ab796
TatSmat <smatova.tatiana@gmail.com>
2021-09-29 11:01:58 +0300

    Iteration 4. Add detailed view corrected some errors

--- lesson/urls.py
+++ lesson/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'lesson'
 urlpatterns = [
-    path('', views.all_materials, name='all_material'),
+    path('', views.all_materials, name='all_materials'),
     path('<int:y>/<int:m>/<int:d>/<slug:slug>', views.detailed_material, name='detailed_material'),
 ]
